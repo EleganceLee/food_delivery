bdc287c9ff1b7923cacbd721da2464660e339b5f
EleganceLee <elegance.mdev02@gmail.com>
2022-11-20 21:52:24 +0700

    add map and search

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,71 +15,38 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final double sw = MediaQuery.of(context).size.width;
+    final double sh = MediaQuery.of(context).size.height;
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(80.0),
         child: AppBar(
           leading: Padding(
             padding: const EdgeInsets.only(left: 8.0),
             child: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  "https://s359.kapook.com/r/600/auto/pagebuilder/c77a4980-a5a6-4d23-8e61-48ea58f25879.jpg"),
+              backgroundColor: Colors.white,
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://s359.kapook.com/r/600/auto/pagebuilder/c77a4980-a5a6-4d23-8e61-48ea58f25879.jpg"),
+              ),
             ),
           ),
           centerTitle: false,
@@ -95,6 +64,7 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ],
           ),
+          excludeHeaderSemantics: true,
           elevation: 0.0,
           backgroundColor: Colors.transparent,
           flexibleSpace: Container(
@@ -105,56 +75,216 @@ class _MyHomePageState extends State<MyHomePage> {
                 color: Color.fromARGB(255, 0, 95, 248)),
           ),
           actions: [
-            Padding(
+            Container(
+              height: 60,
+              width: sw * 0.3,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 4, 16, 5),
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(
+                  width: 4,
+                  color: Color.fromARGB(255, 40, 63, 42),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 1,
+                    blurRadius: 7,
+                    offset: Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+              ),
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            "Wallet",
+                            style: TextStyle(
+                                fontSize: 8,
+                                color: Color.fromARGB(255, 195, 188, 188)),
+                          ),
+                          Icon(
+                            Icons.attach_money,
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          const Text(
+                            "780.00",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(width: 10),
+            const Padding(
               padding: const EdgeInsets.only(right: 8.0),
               child: CircleAvatar(
-                radius: 25,
-                backgroundColor: Color.fromARGB(255, 0, 0, 0),
+                backgroundColor: Color.fromARGB(255, 40, 63, 42),
+                radius: 30,
+                child: CircleAvatar(
+                  radius: 25,
+                  backgroundColor: Color.fromARGB(255, 0, 0, 0),
+                  child: Icon(
+                    Icons.notifications_outlined,
+                    size: 30,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Stack(
+        children: [
+          Positioned(
+            child: GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: LatLng(13.7658368, 100.5518848),
+                zoom: 14,
+              ),
+            ),
+          ),
+          Positioned(
+            top: 130 + 20,
+            width: sw,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    height: 60,
+                    width: sw * 0.7,
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(
+                        width: 1,
+                        color: Color.fromARGB(255, 66, 254, 194),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 1,
+                          blurRadius: 7,
+                          offset: Offset(0, 3), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    child: Center(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          prefixIcon: Icon(
+                            Icons.search,
+                            color: Colors.black,
+                          ),
+                          hintText: 'Search Tech Talk',
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 60,
+                    width: sw * 0.2,
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(
+                        width: 1,
+                        color: Color.fromARGB(255, 66, 254, 194),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 1,
+                          blurRadius: 7,
+                          offset: Offset(0, 3), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    child: Center(
+                      child: Icon(
+                        Icons.sort_rounded,
+                        color: Colors.black,
+                        size: 40,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: ClipPath(
+        clipper: FloatingClipPath(),
+        child: Container(
+          height: 60,
+          width: sw * 0.8,
+          decoration: BoxDecoration(
+            color: Color.fromARGB(255, 43, 43, 43),
+            borderRadius: BorderRadius.circular(40),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Icon(
+                Icons.location_on_outlined,
+                size: 40,
+                color: Colors.white,
+              ),
+              CircleAvatar(
+                radius: 100,
+                backgroundColor: Color.fromARGB(255, 202, 149, 149),
                 child: Icon(
-                  Icons.notifications_outlined,
-                  size: 30,
+                  Icons.qr_code_scanner,
+                  size: 40,
                   color: Colors.white,
                 ),
               ),
-            )
-          ],
+              Icon(
+                Icons.person,
+                size: 40,
+                color: Colors.white,
+              ),
+            ],
+          ),
         ),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class FloatingClipPath extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return true;
   }
 }
